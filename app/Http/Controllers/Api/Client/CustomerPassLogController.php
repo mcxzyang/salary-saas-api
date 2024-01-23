9ba16464bfbfef9b73427d9d27383ed12380021e
wfw2113163 <wfw2113@163.com>
2024-01-23 09:56:54 +0800

    同上

--- app/Http/Controllers/Api/Client/CustomerPassLogController.php
+++ app/Http/Controllers/Api/Client/CustomerPassLogController.php
@@ -17,7 +17,7 @@ class CustomerPassLogController extends Controller
 
         $list = CustomerPassLog::filter($request->all())
             ->where('company_id', $user->company_id)
-            ->with(['fromUser', 'toUser', 'customer', 'updatedUser'])
+            ->with(['fromUser', 'toUser', 'customer', 'updatedUser', 'createdUser'])
             ->orderBy('id', 'desc')
             ->paginateOrGet();
         return $this->success(BaseResource::collection($list));
