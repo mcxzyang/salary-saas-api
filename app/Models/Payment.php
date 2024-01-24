8409092a6066e6ea151df3ed32b4308aaec18bfd
mcxzyang <mcxzyang@gmail.com>
2024-01-24 22:50:11 +0800

    Feat: CollectionAccount and Payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -15,7 +15,7 @@ class Payment extends Model
     use LogsActivityTrait;
     use RecordUserTrait;
 
-    protected $fillable = ['company_id', 'order_id', 'total', 'order_total', 'order_already_payed', 'payment_proof', 'pay_at', 'collection_account_id', 'pay_method', 'pay_desc', 'status', 'created_by', 'updated_by'];
+    protected $fillable = ['company_id', 'order_id', 'total', 'order_total', 'order_already_payed', 'payment_proof', 'pay_at', 'collection_account_id', 'pay_method', 'pay_desc', 'status', 'created_by', 'updated_by', 'type'];
 
     protected $casts = [
         'payment_proof' => 'json',
