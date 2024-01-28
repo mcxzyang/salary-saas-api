fac7609eecf658c4dcc750640367dd216893425c
wfw2113163 <wfw2113@163.com>
2024-01-28 21:34:01 +0800

    0

--- app/Models/WorkorderTask.php
+++ app/Models/WorkorderTask.php
@@ -11,7 +11,9 @@ class WorkorderTask extends Model
     use FormatDate;
 
     protected $fillable = ['workorder_id', 'name', 'no', 'working_process_id', 'report_working_rate', 'plan_start_at', 'plan_end_at', 'plan_number', 'actual_start_at', 'actual_end_at',
-        'good_score_number', 'ungood_score_number', 'working_process_charge_user_id', 'report_working_charge_user_id', 'report_working_permission', 'approve_company_user_id', 'status', 'goods_id'];
+        'good_score_number', 'ungood_score_number', 'working_process_charge_user_id', 'report_working_charge_user_id', 'report_working_permission', 'approve_company_user_id', 'status',
+        'goods_id', 'defectives'
+    ];
 
     protected $casts = [
         'plan_start_at' => 'datetime',
